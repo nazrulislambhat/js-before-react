2d0c705decf7373754ad757f37ed8567d326e84c
Nazrul Islam <nazrulislambhat@gmail.com>
2023-06-27 11:30:57 +0530

    hakuna matata

--- async-await/script.js
+++ async-await/script.js
@@ -1,1 +1,22 @@
-console.log('h');
+//sync code example
+function otherFunction() {
+  console.log('Other Function');
+}
+
+console.log('Start');
+otherFunction();
+console.log('End');
+
+//async
+
+
+
+console.log('Start');
+
+
+setTimeout(() => {
+  console.log('ion timeou');
+}, 2000);
+
+console.log('End');
+
